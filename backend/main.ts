a59e4dd8a0f0062468445c0b37c272f36da38f60
James Chiu Wai Chun <jospehy6@gmail.com>
2023-06-28 16:31:13 +0100

    first commit

---
+++ backend/main.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req: Request, res: Response) {
+  res.end("Hello World");
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/`);
+});
