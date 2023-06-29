ccf36ec2fb231b7f33ad9edd7cc9187977f84610
James Chiu Wai Chun <jospehy6@gmail.com>
2023-06-29 16:23:57 +0100

    Created dummy.test.tsx for frontend test case

---
+++ frontend/src/dummy.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe('it is dummy', ()=>{
+    it('should pass', ()=>{
+        expect(1).toBe(1)
+    })
+})
