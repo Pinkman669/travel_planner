12162b8adb8b689d4bcc2e19e0e78c98a3f2e374
James Chiu Wai Chun <jospehy6@gmail.com>
2023-07-04 11:38:48 +0100

    Fixed bug

--- frontend/src/features/auth/SignUp.tsx
+++ frontend/src/features/auth/SignUp.tsx
@@ -5,7 +5,7 @@ import '../../css/SignUp.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { Link } from "react-router-dom";
 import { localSignUp } from "./AuthAPI";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { notify } from '../utils/utils'
 
 
