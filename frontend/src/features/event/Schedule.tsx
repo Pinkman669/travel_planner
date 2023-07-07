09e9e8defa609cecbb08222007f4382d24f49404
James Chiu Wai Chun <jospehy6@gmail.com>
2023-07-07 17:12:45 +0100

    Modified Schedule.tsx

--- frontend/src/features/event/Schedule.tsx
+++ frontend/src/features/event/Schedule.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import styles from '../../css/Schedule.module.css'
 
+interface ScheduleProps{
+    tripName: string;
+    userName: string;
+}
 
-export default function Schedule(){
+export default function Schedule(props: ScheduleProps){
     return (
-        <div>
-            This is schedule
+        <div id={styles.scheduleContainer}>
+            <div id={styles.ScheduleHeader}>
+                <h3 id={styles.tripTitle}>{props.tripName}</h3>
+                <div>{props.userName}</div>
+            </div>
+            <div id={styles.ScheduleLine}></div>
+            <div id={styles.allDaysContainer}>
+                <div className={styles.dayContainer}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/features/event/TripEvent.tsx
+++ frontend/src/features/event/TripEvent.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { useParams } from "react-router-dom";
+import Schedule from "./Schedule";
+import styles from '../../css/TripEvent.module.css'
 
 interface EventItem{
     id: number;
@@ -10,9 +12,12 @@ interface EventItem{
 export default function TripEvent(){
     const {tripId} = useParams()
     const tripInfo = useAppSelector(state => state.trip.tripItems.find((item) => item.id === Number(tripId)))
+    const userName = useAppSelector(state => state.auth.name)
+
     return (
-        <div>
+        <div className={`container-fluid`}>
             This is event: {tripInfo?.name}
+            <Schedule tripName={tripInfo?.name as string} userName={userName as string}/>
         </div>
     )
 }
