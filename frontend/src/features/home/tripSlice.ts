05033f054ac71f4f4ee6779d7045e6f1493eabd6
Naomi Chan <channaomi.b@gmail.com>
2023-07-13 12:06:10 -0400

    export type

--- frontend/src/features/home/tripSlice.ts
+++ frontend/src/features/home/tripSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-interface TripItem {
+export interface TripItem {
     id: number;
     start_date: Date;
     end_date: Date;
