aeb9ee92fdebb475b5dcfd7aee1de5845a2e5b95
Naomi Chan <channaomi.b@gmail.com>
2023-07-17 14:03:50 -0400

    rename files

--- frontend/src/features/featureContainer/Feature.tsx
+++ frontend/src/features/featureContainer/Feature.tsx
@@ -1,7 +1,7 @@
 import { useLoadScript } from "@react-google-maps/api";
 import { Map } from "../map/GoogleMap";
 import FeatureTab from "./FeatureTab";
-import "../../css/Feature.css"
+// import "../../css/Feature.css"
 import FavouriteEvent from "../event/FavouriteEvent";
 
 
