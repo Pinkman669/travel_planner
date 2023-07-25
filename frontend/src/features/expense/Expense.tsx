73b91c2d7e18188c6d6556ace5ed7e1b490a68b5
Naomi Chan <channaomi.b@gmail.com>
2023-07-25 10:22:53 -0400

    modified click and show expense detail

--- frontend/src/features/expense/Expense.tsx
+++ frontend/src/features/expense/Expense.tsx
@@ -7,30 +7,35 @@ import { useState } from "react";
 export function Expense() {
   const { tripId } = useParams();
   const expenseResult = useExpenseInfo(tripId!);
-  const [showBudgetSubTab,setBudgetSubTab] = useState(false)
-  const [showExpenseSubTab,setExpenseSubTab] = useState(false)
+  const [showBudgetSubTab, setBudgetSubTab] = useState(false);
+  const [showExpenseSubTab, setExpenseSubTab] = useState(false);
   const detail = expenseResult?.detail;
-  console.log(`result: ${detail}`)
+  console.log(`result: ${detail}`);
   return (
     <div className="expense-page">
-      <div className="expense-container">
+      <div className="expense-container" onClick={() => setBudgetSubTab(!showBudgetSubTab)}>
         <div>Budget</div>
-        <div>${expenseResult?.totalBudget}</div>
+        <div>
+          ${expenseResult?.totalBudget}
+        </div>
       </div>
       <BudgetSubTab
-      isShown = {showBudgetSubTab}
-      result = {detail!}
-      onHide={() => setBudgetSubTab(false)}/>
+        isShown={showBudgetSubTab}
+        result={detail!}
+        onHide={() => setBudgetSubTab(false)}
+      />
 
-      <div className="expense-container" >
+      <div className="expense-container"onClick={() => setExpenseSubTab(!showExpenseSubTab)}>
         <div>Expense</div>
-        <div>${expenseResult?.totalExpense}</div>
+        <div onClick={() => setExpenseSubTab(true)}>
+          ${expenseResult?.totalExpense}
+        </div>
       </div>
       <ExpenseSubTab
-      isShown = {showExpenseSubTab}
-      result = {detail!}
-      onHide={() => setExpenseSubTab(false)}/>
-
+        isShown={showExpenseSubTab}
+        result={detail!}
+        onHide={() => setExpenseSubTab(false)}
+      />
 
       <div className="expense-container total-container">
         <div>Total</div>
