552124b296e4319b7d7b422fd5dcb06fb9671c2a
James Chiu Wai Chun <jospehy6@gmail.com>
2023-08-16 20:43:16 +0100

    Modified github yml and knexfile.ts

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -26,6 +26,7 @@ const config: { [key: string]: Knex.Config } = {
 			database: process.env.TESTDB_NAME,
 			user: process.env.POSTGRES_USER,
 			password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST
 		},
 		pool: {
 			min: 2,
